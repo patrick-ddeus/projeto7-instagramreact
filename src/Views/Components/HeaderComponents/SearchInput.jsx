1fc2a62d8a3797ad1875a725a53a86799f30252c
PatKat20 <patrickdev4@gmail.com>
2023-02-07 02:45:40 -0300

    Novos arquivos de build

--- src/Views/Components/HeaderComponents/SearchInput.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SearchInput = () => (
-    <form action="#" className="h-form">
-        <input type="text" name="" id="h-input" placeholder="Pesquisar" />
-    </form>
-)
-
-export default SearchInput
